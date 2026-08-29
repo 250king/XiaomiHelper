af52641820e2b76d0951e99679cc36e1e4063a4b
耀 <250king.top@gmail.com>
2026-08-29 15:32:16 +0800

    feat: wire default-browser redirect into MiMirror

--- app/src/main/kotlin/dev/lackluster/mihelper/hook/scopes/MiMirror.kt
+++ app/src/main/kotlin/dev/lackluster/mihelper/hook/scopes/MiMirror.kt
@@ -2,11 +2,13 @@ package dev.lackluster.mihelper.hook.scopes
 
 import dev.lackluster.mihelper.hook.base.StaticHooker
 import dev.lackluster.mihelper.hook.rules.mimirror.ContinueTasks
+import dev.lackluster.mihelper.hook.rules.shared.XiaomiBrowserRedirect
 
 object MiMirror : StaticHooker() {
     override val requireDexKit: Boolean = true
 
     override fun onInit() {
         attach(ContinueTasks)
+        attach(XiaomiBrowserRedirect)
     }
 }
