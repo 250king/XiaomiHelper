2ca3c75656c90cab06fbc4d3bdc91dd081bfc745
耀 <250king.top@gmail.com>
2026-08-29 14:59:47 +0800

    feat: attach device-name validation bypass

--- app/src/main/kotlin/dev/lackluster/mihelper/hook/scopes/Settings.kt
+++ app/src/main/kotlin/dev/lackluster/mihelper/hook/scopes/Settings.kt
@@ -1,6 +1,7 @@
 package dev.lackluster.mihelper.hook.scopes
 
 import dev.lackluster.mihelper.hook.base.StaticHooker
+import dev.lackluster.mihelper.hook.rules.settings.DisableDeviceNameCheck
 import dev.lackluster.mihelper.hook.rules.settings.FontScale
 import dev.lackluster.mihelper.hook.rules.settings.HeaderList
 import dev.lackluster.mihelper.hook.rules.settings.QuickPermission
@@ -10,5 +11,6 @@ object Settings : StaticHooker() {
         attach(FontScale)
         attach(HeaderList)
         attach(QuickPermission)
+        attach(DisableDeviceNameCheck)
     }
 }
