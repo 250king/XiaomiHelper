2040342efdb4bab491615271c64117fe31bb3bad
耀 <250king.top@gmail.com>
2026-08-29 14:47:12 +0800

    feat: suppress clipboard usage toast

--- app/src/main/kotlin/dev/lackluster/mihelper/hook/rules/lbe/ClipboardToast.kt
+++ app/src/main/kotlin/dev/lackluster/mihelper/hook/rules/lbe/ClipboardToast.kt
@@ -20,52 +20,29 @@
 
 package dev.lackluster.mihelper.hook.rules.lbe
 
-import android.content.Context
-import android.widget.Toast
 import com.highcapable.kavaref.KavaRef.Companion.resolve
-import dev.lackluster.mihelper.data.Scope
 import dev.lackluster.mihelper.data.preference.Preferences
 import dev.lackluster.mihelper.hook.base.StaticHooker
 import dev.lackluster.mihelper.hook.utils.RemotePreferences.get
-import dev.lackluster.mihelper.hook.utils.toTyped
-import dev.lackluster.mihelper.utils.factory.getResId
+import dev.lackluster.mihelper.hook.utils.toClassOrNull
 
 object ClipboardToast : StaticHooker() {
-    private var overlay_read_clip_toast = 0
-
     override fun onInit() {
         updateSelfState(Preferences.LBE.TOAST_CLIPBOARD_USAGE.get())
     }
 
     override fun onHook() {
         "com.lbe.security.utility.ToastUtil".toClassOrNull()?.apply {
-            val mContext = resolve().firstFieldOrNull {
-                name = "mContext"
-            }?.toTyped<Context>()
             resolve().firstMethodOrNull {
                 name = "initToastView"
             }?.hook {
-                val type = getArg(1) as? Int
-                if (type == 1) {
-                    val context = mContext?.get(thisObject)
-                    val pkgName = getArg(0) as? String
-                    if (context != null && pkgName != null) {
-                        if (overlay_read_clip_toast == 0) {
-                            overlay_read_clip_toast = context.getResId("overlay_read_clip_toast", "string", Scope.LBE)
-                        }
-                        context.packageManager.let {
-                            it.getPackageInfo(pkgName, 0).applicationInfo?.loadLabel(it)?.toString()
-                        }?.let {
-                            Toast.makeText(
-                                context,
-                                context.getString(overlay_read_clip_toast, it),
-                                Toast.LENGTH_SHORT
-                            ).show()
-                            return@hook result(null)
-                        }
-                    }
+                // type == 1 is the clipboard-read usage prompt. Suppress only this
+                // LBE notification instead of globally blocking Toasts.
+                if ((getArg(1) as? Int) == 1) {
+                    result(null)
+                } else {
+                    result(proceed())
                 }
-                result(proceed())
             }
         }
     }
