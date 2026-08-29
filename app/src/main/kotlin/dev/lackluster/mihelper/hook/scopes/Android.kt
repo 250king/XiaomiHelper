13517dbbe5579c8ebaffaeb0ba4278b46a99009f
耀 <250king.top@gmail.com>
2026-08-29 14:49:36 +0800

    fix: remove stray FCM import

--- app/src/main/kotlin/dev/lackluster/mihelper/hook/scopes/Android.kt
+++ app/src/main/kotlin/dev/lackluster/mihelper/hook/scopes/Android.kt
@@ -4,7 +4,6 @@ import dev.lackluster.mihelper.hook.base.StaticHooker
 import dev.lackluster.mihelper.hook.rules.android.AllowMoreFreeform
 import dev.lackluster.mihelper.hook.rules.android.DarkModeForAll
 import dev.lackluster.mihelper.hook.rules.android.DisableWakePathChecker
-import dev.lackluster.mihelper.hook.rules.android.FCMAutoStart
 import dev.lackluster.mihelper.hook.rules.android.FCMSystemFix
 import dev.lackluster.mihelper.hook.rules.android.FontScale
 import dev.lackluster.mihelper.hook.rules.android.RemoveFreeformRestriction
