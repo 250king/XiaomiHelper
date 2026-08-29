b368f1f14f89486615a87c430fbadb06563d339d
耀 <250king.top@gmail.com>
2026-08-29 17:17:58 +0800

    fix: remove invalid toClassOrNull import

--- app/src/main/kotlin/dev/lackluster/mihelper/hook/rules/android/FCMSystemFix.kt
+++ app/src/main/kotlin/dev/lackluster/mihelper/hook/rules/android/FCMSystemFix.kt
@@ -5,7 +5,6 @@ import com.highcapable.kavaref.KavaRef.Companion.resolve
 import dev.lackluster.mihelper.data.preference.Preferences
 import dev.lackluster.mihelper.hook.base.StaticHooker
 import dev.lackluster.mihelper.hook.utils.RemotePreferences.get
-import dev.lackluster.mihelper.hook.utils.toClassOrNull
 
 /**
  * System-server side companion of [dev.lackluster.mihelper.hook.rules.powerkeeper.GMSBackgroundRunning].
